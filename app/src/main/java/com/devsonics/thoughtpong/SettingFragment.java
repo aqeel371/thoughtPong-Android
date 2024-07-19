2c433d31c4706aab143a06449fb5e8b012355bd2
Muhammad Aqeel <muhammadaqeel.sgd2@gmail.com>
2024-07-19 21:20:38 +0500

    logout

--- app/src/main/java/com/devsonics/thoughtpong/SettingFragment.java
+++ app/src/main/java/com/devsonics/thoughtpong/SettingFragment.java
@@ -1,26 +1,35 @@
 package com.devsonics.thoughtpong;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SeekBar;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.devsonics.thoughtpong.activities.login.Login;
+import com.devsonics.thoughtpong.retofit_api.request_model.RequestCall;
 import com.devsonics.thoughtpong.utils.SharedPreferenceManager;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class SettingFragment extends Fragment {
     SeekBar seekBarVolume;
     Switch aSwitchStartupVolume;
-    ConstraintLayout btnPrivacyPolicy, btnTermsConditions, btnContactUs, btnShareApp, btnLogout;
+    ConstraintLayout btnPrivacyPolicy, btnTermsConditions, btnContactUs, btnLogout;
 
 
     public SettingFragment() {
@@ -37,7 +46,6 @@ public class SettingFragment extends Fragment {
         btnPrivacyPolicy = view.findViewById(R.id.btn_privacy_policy);
         btnTermsConditions = view.findViewById(R.id.btn_terms_conditions);
         btnContactUs = view.findViewById(R.id.btn_contact_us);
-        btnShareApp = view.findViewById(R.id.btn_share_app);
         btnLogout = view.findViewById(R.id.btn_logout);
         btnPrivacyPolicy.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,11 +55,19 @@ public class SettingFragment extends Fragment {
         });
 
         btnLogout.setOnClickListener(v -> {
-            FirebaseAuth.getInstance().signOut();
-            SharedPreferenceManager.INSTANCE.setUserData(null);
-            SharedPreferenceManager.INSTANCE.setUserLogin(false);
-            startActivity(new Intent(requireActivity(), Login.class));
-            requireActivity().finishAffinity();
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+            builder.setMessage("Are you sure to logout?").setPositiveButton("Yes", (dialog, which) -> {
+                        FirebaseAuth.getInstance().signOut();
+                        SharedPreferenceManager.INSTANCE.setUserData(null);
+                        SharedPreferenceManager.INSTANCE.setUserLogin(false);
+                        startActivity(new Intent(requireActivity(), Login.class));
+                        requireActivity().finishAffinity();
+                        dialog.dismiss();
+                    }).setNegativeButton("No", (dialog, which) -> {
+
+                        dialog.dismiss();
+                    }).setCancelable(false) // Prevent dialog from being dismissed by clicking outside
+                    .show();
         });
 
         return view;
